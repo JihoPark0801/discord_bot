3e649af7a9dbb2dd72fd84d16b65d3e06441d429
JihoPark0801 <jihopark2002@gmail.com>
2025-11-17 20:37:33 -0800

    1: got the bot online

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import discord
+import os
+from dotenv import load_dotenv
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello!')
+
 def main():
-    print("Hello from discord-bot!")
-
-
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = Client(intents=intents)
+    load_dotenv()
+    client.run(os.getenv('DISCORD_BOT_TOKEN'))
+    
 if __name__ == "__main__":
     main()
